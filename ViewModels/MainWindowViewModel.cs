2a8b11dd618496f00852e06973d46e6b3448181d
MyouKin <2517630545@qq.com>
2025-07-31 00:14:37 +0800

    init

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace ROSBox.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ROSBox.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}

676d20ac3a2e87cd4cc1ed08e44365803f5910eb
OneCraftZRX <2517630545@qq.com>
2025-08-13 00:59:57 +0800

    update

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,23 @@
-﻿namespace ROSBox.ViewModels;
+﻿using System.Collections.ObjectModel;
+
+namespace ROSBox.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    public class Node
+    {
+        public ObservableCollection<Node>? SubNodes { get; }
+        public string Title { get; }
+    
+        public Node(string title)
+        {
+            Title = title;
+        }
+
+        public Node(string title, ObservableCollection<Node> subNodes)
+        {
+            Title = title;
+            SubNodes = subNodes;
+        }
+    }
 }
--- ViewModels/ViewModelBase.cs
+++ ViewModels/ViewModelBase.cs
@@ -1,7 +1,23 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace ROSBox.ViewModels;
 
 public class ViewModelBase : ObservableObject
 {
+    public ObservableCollection<MainWindowViewModel.Node> Nodes{ get; }
+
+    public ViewModelBase()
+    {
+        Nodes = new ObservableCollection<MainWindowViewModel.Node>
+        {                
+            new MainWindowViewModel.Node("Animals", new ObservableCollection<MainWindowViewModel.Node>
+            {
+                new MainWindowViewModel.Node("Mammals", new ObservableCollection<MainWindowViewModel.Node>
+                {
+                    new MainWindowViewModel.Node("Lion"), new MainWindowViewModel.Node("Cat"), new MainWindowViewModel.Node("Zebra")
+                })
+            })
+        };
+    }
 }
